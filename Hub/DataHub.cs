a1aec6e82d6e1ef22dd1455cf1668e949c59eabd
Mohamed Adel <inbox.adel@gmail.com>
2024-05-05 12:19:17 +0200

    Adding data Hubs For signalR

---
+++ Hub/DataHub.cs
@@ -0,0 +1,17 @@
+﻿using GeoMapping.DTO;
+using GeoMapping.Models;
+using Microsoft.AspNetCore.SignalR;
+using GeoMapping.Hub;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hub
+{
+    public class DataHub : Hub<IDataHubClient>
+    {
+        public async Task SendStocksToUser(List<AddressDTO> data)
+        {                                                                                                   
+            await Clients.All.SendAddressesToTable(data);
+        }
+    }
+}
---
+++ Hub/IDataHub.cs
@@ -0,0 +1,13 @@
+﻿using GeoMapping.DTO;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeoMapping.Hub
+{
+    public interface IDataHubClient
+    {
+        Task SendAddressesToTable (List<AddressDTO> addressDTOs);
+    }
+}
+
